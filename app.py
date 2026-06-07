760372cd3f6a982c3cd92618dd9c269b2dbeac3a
Charles <cgray888@gmail.com>
2026-06-07 21:19:42 +1000

    Added home page, rules page basic layouts with working links.

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World'
+    return render_template('home.html')
+
+@app.route('/rules')
+def display_rules():
+    return render_template('rules.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
