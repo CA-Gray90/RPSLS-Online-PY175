fb57e9b3a14abc2e36cb2e4cb572e9845f90495b
Charles <cgray888@gmail.com>
2026-06-20 11:37:50 +1000

    Added templates for sign up pages, leaderboard, etc

--- app.py
+++ app.py
@@ -10,5 +10,19 @@ def home():
 def display_rules():
     return render_template('rules.html')
 
+@app.route('/sign_in')
+def sign_in():
+    return render_template('sign_in.html')
+
+@app.route('/play')
+def play_game():
+    return render_template('play_game.html')
+
+@app.route('/leaderboard')
+def display_leaderboard():
+    # Retrieve leadboard file
+    # display leaderboard > pass it to template
+    return render_template('leaderboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
